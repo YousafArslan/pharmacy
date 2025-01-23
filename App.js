8b88b4f402b98bb09ebc06c2f92dc9e9b33e1f8f
azwar <azwar@maindata.com>
2025-01-23 20:11:35 +0500

    Facing trouble again

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './src/redux/store';
 import RootNavigator from './src/routes/RootNavigator';
+import "react-native-devsettings";
 
 const App = () => {
   console.log('Debugger connected!');
